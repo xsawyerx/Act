95b236f49393531621b4bb15bfb9849cb2423905
Sawyer X <xsawyerx@cpan.org>
2014-12-29 00:31:35 +0100

    around BUILDARGS

--- lib/Act/Role/HasDateTime.pm
+++ lib/Act/Role/HasDateTime.pm
@@ -1,22 +1,22 @@
 package Act::Role::HasDateTime;
+
 # ABSTRACT: A role that automatically expands DateTime objects
 
 use Moo::Role;
 use DateTime::Format::Pg;
 
-# FIXME: around should be used here instead
-sub BUILDARGS {
+around BUILDARGS => sub {
+    my $orig  = shift;
     my $class = shift;
     my %args  = @_ == 1 ? %{ $_[0] } : @_;
 
     # expand datetime
     if ( defined $args{'datetime'} ) {
-        $args{'datetime'} = DateTime::Format::Pg->parse_datetime(
-            $args{'datetime'}
-        );
+        $args{'datetime'}
+            = DateTime::Format::Pg->parse_datetime( $args{'datetime'} );
     }
 
     return \%args;
-}
+};
 
 1;
